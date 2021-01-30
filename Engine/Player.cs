5131f768e84341e9667bf37ef0d7ec24545ada94
Lucas Levi <lucas.lsl12@gmail.com>
2021-01-30 20:11:03 -0300

    Define Player Class

---
+++ Engine/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Player // Public because all the game need to comunicate with the Player Class
+    {
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int Gold { get; set; }
+        public int ExperiencePonts { get; set; }
+        public int Level { get; set; }
+    }
+}
