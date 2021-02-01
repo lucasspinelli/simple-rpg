0660e7f4a732960c7caeec1f8a15672a43c5a5b2
Lucas Levi <lucas.lsl12@gmail.com>
2021-01-31 21:48:11 -0300

    Heal class

---
+++ Engine/HealingPotion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class HealingPotion
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set; } //Sometimes is more then one 
+        public int AmountToHeal { get; set; }
+
+    }
+}
