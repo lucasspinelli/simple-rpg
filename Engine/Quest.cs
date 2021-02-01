1faf841daab3eb6ac3c23bd573db00bb33367f90
Lucas Levi <lucas.lsl12@gmail.com>
2021-02-01 16:52:34 -0300

    Apply inheterance on Weapon and HealingPotion

--- Engine/Quest.cs
+++ Engine/Quest.cs
@@ -8,5 +8,10 @@ namespace Engine
 {
     class Quest
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
     }
 }
--- Engine/Weapon.cs
+++ Engine/Weapon.cs
@@ -8,5 +8,10 @@ namespace Engine
 {
     class Weapon
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set; }
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
     }
 }
