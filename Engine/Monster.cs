6199ebf6e1886e506cf5ecdc0806780845da6ee6
Lucas Levi <lucas.lsl12@gmail.com>
2021-01-31 21:54:22 -0300

    Monster Class

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    class Location
+    class Monster
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int CurrentHitPoints { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
     }
 }
